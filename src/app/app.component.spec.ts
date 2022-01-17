e44985c073ee3f05c8fe9e49ab0115c71e8a0ce4
theNoobCoder123 <patelhrishikesh2000@gmail.com>
2022-01-17 23:42:38 +0530

    Renamed project to 'GettingStartedWithAngular' old name was 'simpleFormWithLayout'.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'simpleFormWithLayout'`, () => {
+  it(`should have as title 'GettingStartedWithAngular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('simpleFormWithLayout');
+    expect(app.title).toEqual('GettingStartedWithAngular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('simpleFormWithLayout app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('GettingStartedWithAngular app is running!');
   });
 });
